023382c6aba8b61eac0f9c4f17e4402d3512ae9d
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-06-16 14:13:51 -0700

    this worked, but have some missing dates

--- v0/data/submit_ufs2arco.sh
+++ v0/data/submit_ufs2arco.sh
@@ -3,13 +3,13 @@
 #SBATCH -J nested-eagle-v0-data
 #SBATCH -o slurm/preprocessing.%j.out
 #SBATCH -e slurm/preprocessing.%j.err
-#SBATCH --nodes=8
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=64
 #SBATCH --cpus-per-task=4
-#SBATCH --qos=debug
+#SBATCH --qos=regular
 #SBATCH --account=m4718
 #SBATCH --constraint=cpu
-#SBATCH -t 00:30:00
+#SBATCH -t 02:00:00
 
 module load conda
 conda activate ufs2arco
@@ -17,6 +17,6 @@ export PYTHONPATH=""
 python create_grids.py
 
 srun ufs2arco gfs.analysis.training.yaml --overwrite
-srun ufs2arco gfs.forecast.training.yaml --overwrite
-srun ufs2arco hrrr.analysis.training.yaml --overwrite
-srun ufs2arco hrrr.forecast.training.yaml --overwrite
+#srun ufs2arco gfs.forecast.training.yaml --overwrite
+#srun ufs2arco hrrr.analysis.training.yaml --overwrite
+#srun ufs2arco hrrr.forecast.training.yaml --overwrite
