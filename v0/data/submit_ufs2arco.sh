97013599836aac79e840a75228b7bcc728876959
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-07-14 12:43:33 -0700

    after-the-fact saving to community

--- v0/data/submit_ufs2arco.sh
+++ v0/data/submit_ufs2arco.sh
@@ -24,3 +24,19 @@ srun ufs2arco hrrr.analysis.yaml --overwrite
 echo "done with hrrr analysis"
 srun ufs2arco hrrr.forecast.yaml --overwrite
 echo "done with hrrr forecast"
+
+# Now copy some things over to community
+mywork=$WORK/nested-eagle/v0/data
+mycomm=$COMMUNITY/nested-eagle/v0/data
+mkdir -p $mycomm
+mkdir -p $mycomm/logs/gfs-analysis
+mkdir -p $mycomm/logs/gfs-forecast
+mkdir -p $mycomm/logs/hrrr-analysis
+mkdir -p $mycomm/logs/hrrr-forecast
+
+cp -v $mywork/*.nc $mycomm
+cp -v $mywork/*.yaml $mycomm
+cp -v $mywork/logs/gfs-analysis/log.0000.*.out $mycomm/logs/gfs-analysis
+cp -v $mywork/logs/gfs-forecast/log.0000.*.out $mycomm/logs/gfs-forecast
+cp -v $mywork/logs/hrrr-analysis/log.0000.*.out $mycomm/logs/hrrr-analysis
+cp -v $mywork/logs/hrrr-forecast/log.0000.*.out $mycomm/logs/hrrr-forecast
