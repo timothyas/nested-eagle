6110328de108f79c6ac8f2d1fcf0caa4fd8c8afc
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-07-17 15:56:13 -0700

    inference with funky smswt-custom

--- v0/10percent/smswt-custom/inference/run_inference.py
+++ v0/10percent/smswt-custom/inference/run_inference.py
@@ -3,7 +3,7 @@ import sys
 _nested_eagle = os.path.expandvars("${HOME}/nested-eagle")
 sys.path.append(_nested_eagle)
 
-import eagle
+import eagle.inference
 
 if __name__ == "__main__":
-    eagle.run_inference()
+    eagle.inference.run_inference()
