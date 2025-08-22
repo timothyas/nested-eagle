9d5f1322e61c2a4d72b542a0b0e562b0c60abe92
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-08-21 21:58:10 -0700

    global rmse for 1 deg global model

---
+++ v0/baselines/gfs-forecasts-vs-gfs-analysis/submit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -J gfs-forecasts
+#SBATCH -o gfs-forecasts.%j.out
+#SBATCH -e gfs-forecasts.%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --cpus-per-task=4
+#SBATCH --qos=debug
+#SBATCH --account=m4718
+#SBATCH --constraint=cpu
+#SBATCH -t 00:30:00
+
+module load conda
+conda activate ufs2arco
+
+srun ufs2arco data.yaml
+python compute_metrics.py metrics.yaml
